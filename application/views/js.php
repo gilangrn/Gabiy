fbc4a76d47799456102c5378460cf25813297d69
ivalrivall <ivalrival95@gmail.com>
2018-03-28 11:56:08 +0700

    percabangan view
    membagikan dashboard ke beberapa masterview

---
+++ application/views/js.php
@@ -0,0 +1,27 @@
+
+<!-- BEGIN VENDOR JS-->
+<script src="assets/vendors/js/core/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/prism.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/jquery.matchHeight-min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/screenfull.min.js" type="text/javascript"></script>
+<script src="assets/vendors/js/pace/pace.min.js" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<script src="assets/vendors/js/chartist.min.js" type="text/javascript"></script>
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN APEX JS-->
+<script src="assets/js/app-sidebar.js" type="text/javascript"></script>
+<script src="assets/js/notification-sidebar.js" type="text/javascript"></script>
+<script src="assets/js/customizer.js" type="text/javascript"></script>
+<!-- END APEX JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<script src="assets/js/dashboard1.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL JS-->
+</body>
+
+<!-- Mirrored from pixinvent.com/apex-angular-4-bootstrap-admin-template/html-demo-2/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 22 Mar 2018 10:55:36 GMT -->
+
+</html>
---
+++ application/views/settingmenu.php
@@ -0,0 +1,129 @@
+<!-- Theme customizer Starts-->
+<div class="customizer border-left-blue-grey border-left-lighten-4 d-none d-sm-none d-md-block">
+	<a class="customizer-close">
+		<i class="ft-x font-medium-3"></i>
+	</a>
+	<a id="customizer-toggle-icon" class="customizer-toggle bg-danger">
+		<i class="ft-settings font-medium-4 fa fa-spin white align-middle"></i>
+	</a>
+	<div data-ps-id="df6a5ce4-a175-9172-4402-dabd98fc9c0a" class="customizer-content p-3 ps-container ps-theme-dark">
+		<h4 class="text-uppercase mb-0 text-bold-400">Theme Customizer</h4>
+		<p>Customize & Preview in Real Time</p>
+		<hr>
+		<!-- Sidebar Options Starts-->
+		<h6 class="text-center text-bold-500 mb-3 text-uppercase">Sidebar Color Options</h6>
+		<div class="cz-bg-color">
+			<div class="row p-1">
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="pomegranate" class="gradient-pomegranate d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="king-yna" class="gradient-king-yna d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="ibiza-sunset" class="gradient-ibiza-sunset d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="flickr" class="gradient-flickr d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="purple-bliss" class="gradient-purple-bliss d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="man-of-steel" class="gradient-man-of-steel d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="purple-love" class="gradient-purple-love d-block rounded-circle"></span>
+				</div>
+			</div>
+			<div class="row p-1">
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="black" class="bg-black d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="white" class="bg-grey d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="primary" class="bg-primary d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="success" class="bg-success d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="warning" class="bg-warning d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="info" class="bg-info d-block rounded-circle"></span>
+				</div>
+				<div class="col">
+					<span style="width:20px; height:20px;" data-bg-color="danger" class="bg-danger d-block rounded-circle"></span>
+				</div>
+			</div>
+		</div>
+		<!-- Sidebar Options Ends-->
+		<hr>
+		<!-- Sidebar BG Image Starts-->
+		<h6 class="text-center text-bold-500 mb-3 text-uppercase">Sidebar Bg Image</h6>
+		<div class="cz-bg-image row">
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/01.jpg" width="90" class="rounded">
+			</div>
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/02.jpg" width="90" class="rounded">
+			</div>
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/03.jpg" width="90" class="rounded">
+			</div>
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/04.jpg" width="90" class="rounded">
+			</div>
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/05.jpg" width="90" class="rounded">
+			</div>
+			<div class="col mb-3">
+				<img src="assets/img/sidebar-bg/06.jpg" width="90" class="rounded">
+			</div>
+		</div>
+		<!-- Sidebar BG Image Ends-->
+		<hr>
+		<!-- Sidebar BG Image Toggle Starts-->
+		<div class="togglebutton">
+			<div class="switch">
+				<span>Sidebar Bg Image</span>
+				<div class="float-right">
+					<div class="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
+						<input id="sidebar-bg-img" type="checkbox" checked="" class="custom-control-input cz-bg-image-display">
+						<label for="sidebar-bg-img" class="custom-control-label"></label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Sidebar BG Image Toggle Ends-->
+		<hr>
+		<!-- Compact Menu Starts-->
+		<div class="togglebutton">
+			<div class="switch">
+				<span>Compact Menu</span>
+				<div class="float-right">
+					<div class="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
+						<input id="cz-compact-menu" type="checkbox" class="custom-control-input cz-compact-menu">
+						<label for="cz-compact-menu" class="custom-control-label"></label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Compact Menu Ends-->
+		<hr>
+		<!-- Sidebar Width Starts-->
+		<div>
+			<label for="cz-sidebar-width">Sidebar Width</label>
+			<select id="cz-sidebar-width" class="custom-select cz-sidebar-width float-right">
+				<option value="small">Small</option>
+				<option value="medium" selected="">Medium</option>
+				<option value="large">Large</option>
+			</select>
+		</div>
+		<!-- Sidebar Width Ends-->
+	</div>
+</div>
+<!-- Theme customizer Ends-->
