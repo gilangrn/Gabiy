2d580d6e1085aee640c8b16331a7b8a26278071c
ivalrivall <ivalrival95@gmail.com>
2018-04-04 16:35:16 +0700

    penambahan customer device
    controller customer device dan viewnya

---
+++ application/controllers/CustomerDevice.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CustomerDevice extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('head');
+		$this->load->view('leftmenu');
+		$this->load->view('headmenu');
+		$this->load->view('customer_device');
+		$this->load->view('rightmenu');
+		$this->load->view('settingmenu');
+		$this->load->view('js');
+	}
+}
--- application/views/customers.php
+++ application/views/customers.php
@@ -171,4 +171,4 @@
     <p class="clearfix text-muted text-sm-center px-2"><span>Copyright  &copy; 2018 <a href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" id="pixinventLink" target="_blank" class="text-bold-800 primary darken-2">PIXINVENT </a>, All rights reserved. </span></p>
   </footer>
 </div>
-<!-- </div> -->
+<!-- </div> jangan diapus dulu, dikomen saja-->
