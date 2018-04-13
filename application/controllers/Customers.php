c2855fe70eba7b41e72bbef67648261679521516
ivalrivall <ivalrival95@gmail.com>
2018-04-13 17:44:32 +0700

    bismillah role login v2
    role login v2

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -2,18 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Customers extends CI_Controller {
 	
-	function __construct(){
-		parent::__construct();
-		if($this->session->userdata('level') <> '2')
-		{
-			redirect('login');
-		}
-	}
 		public function index()
 		{
 			$this->load->view('head');
 			$this->load->view('menu');
-			$data['username'] = $this->session->userdata('username');
 			$data['customer'] = $this->customer_model->tampil_data_customer();
 			$this->load->view('customers',$data);
 			$this->load->view('footer');
