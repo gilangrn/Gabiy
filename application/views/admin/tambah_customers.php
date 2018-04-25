533343fc2e276df27519c79bcdf767519e96127a
ivalrivall <ivalrival95@gmail.com>
2018-04-25 17:02:48 +0700

    delete customer

--- application/views/admin/tambah_customers.php
+++ application/views/admin/tambah_customers.php
@@ -77,127 +77,127 @@
                               <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
 
                                 <button type="button" class="btn btn-raised btn-outline-success" data-toggle="modal" data-target="#modal_addto<?php echo $username;?>"><i class="fa fa-plus"></i> Add to Customer</button>
-                                <button type="button" id="" class="btn btn-raised btn-danger" data-toggle="modal" data-target="#modal_hapus<?php echo $username;?>">
+                                <button type="button" id="" class="btn btn-raised btn-outline-danger" data-toggle="modal" data-target="#modal_hapus<?php echo $username;?>">
                                   <i class="ft-x"></i> Delete
                                 </button>
-                            </div>
-                          </td>
-                        </tr>
-                      <?php endforeach;?>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <!-- ============ modal add to Customer ============ -->
-              <?php 
-              foreach($users->result_array() as $i):
-                $username=$i['username']; 
-
-                ?>
-              <div class="modal fade text-left" id="modal_addto<?php echo $username;?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel35" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h3 class="modal-title">Add To Customer</h3>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <?php echo form_open('admin/tambah_data_customer',array('class'=>'form-horizontal','method'=>'post')); ?>
-                    <div class="modal-body">
-                      <div class="row">
-                        <div>
-                          <div class="form-group">
-                            <input type="hidden" class="form-control" name="username" id="username" value="<?php echo $username;?>">
-                          </div>
-                        </div>
-                        <div>
-                          <div class="form-group">
-                            <input type="hidden" class="form-control" name="customer_id" id="customer_id" value="<?php echo $customer_id;?>">
-                          </div>
-                        </div>
-                        <div class="col-8">
-                          <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Customer Name">
-                          </div>
-                        </div>
-                        <div class="col-4">
-                          <div class="form-group">
-                            <label>IP Address</label>
-                            <input type="text" class="form-control" name="ip_address" id="ip_address" placeholder="IP Address">
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-6">
-                          <div class="form-group">
-                            <label>Contact</label>
-                            <input type="text" class="form-control" name="contact_person" id="contact_person" placeholder="Contact Person">
-                          </div>
-                        </div>
-                        <div class="col-6">
-                          <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Email Address">
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-12">
-                          <fieldset class="form-group">
-                            <label for="address">Address</label>
-                            <textarea class="form-control" name="address" id="address" rows="3" placeholder="Full Address"></textarea>
-                          </fieldset>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <input type="reset" class="btn btn-outline-secondary btn-lg" data-dismiss="modal" value="Close">
-                      <input type="submit" class="btn btn-outline-primary btn-lg" value="Add">
-                    </div>
-                    <?php echo form_close() ?>
+                              </div>
+                            </td>
+                          </tr>
+                        <?php endforeach;?>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
-              </div>
-              <?php endforeach;?>
-              <!-- </div> -->
-              <!-- ============ akhir modal add to customer ============-->
-              <!-- ============ modal add new users ============-->
-              <div class="modal fade text-left" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel35" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h3 class="modal-title">Add User</h3>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
+                <!-- ============ modal add to Customer ============ -->
+                <?php 
+                foreach($users->result_array() as $i):
+                  $username=$i['username']; 
+
+                  ?>
+                  <div class="modal fade text-left" id="modal_addto<?php echo $username;?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel35" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h3 class="modal-title">Add To Customer</h3>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <?php echo form_open('admin/tambah_data_customer',array('class'=>'form-horizontal','method'=>'post')); ?>
+                        <div class="modal-body">
+                          <div class="row">
+                            <div>
+                              <div class="form-group">
+                                <input type="hidden" class="form-control" name="username" id="username" value="<?php echo $username;?>">
+                              </div>
+                            </div>
+                            <div>
+                              <div class="form-group">
+                                <input type="hidden" class="form-control" name="customer_id" id="customer_id" value="<?php echo $customer_id;?>">
+                              </div>
+                            </div>
+                            <div class="col-8">
+                              <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Customer Name">
+                              </div>
+                            </div>
+                            <div class="col-4">
+                              <div class="form-group">
+                                <label>IP Address</label>
+                                <input type="text" class="form-control" name="ip_address" id="ip_address" placeholder="IP Address">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-6">
+                              <div class="form-group">
+                                <label>Contact</label>
+                                <input type="text" class="form-control" name="contact_person" id="contact_person" placeholder="Contact Person">
+                              </div>
+                            </div>
+                            <div class="col-6">
+                              <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email Address">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-12">
+                              <fieldset class="form-group">
+                                <label for="address">Address</label>
+                                <textarea class="form-control" name="address" id="address" rows="3" placeholder="Full Address"></textarea>
+                              </fieldset>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <input type="reset" class="btn btn-outline-secondary btn-lg" data-dismiss="modal" value="Close">
+                          <input type="submit" class="btn btn-outline-primary btn-lg" value="Add">
+                        </div>
+                        <?php echo form_close() ?>
+                      </div>
                     </div>
-                    <!-- form modal add users-->
-                    <?php echo form_open('admin/tambah_data_user',array('class'=>'form-horizontal','method'=>'post')); ?>
-                    <div class="modal-body">
-                      <div class="row">
-                        <div class="col-12">
-                          <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
-                          </div>
-                        </div>
+                  </div>
+                <?php endforeach;?>
+                <!-- </div> -->
+                <!-- ============ akhir modal add to customer ============-->
+                <!-- ============ modal add new users ============-->
+                <div class="modal fade text-left" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel35" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h3 class="modal-title">Add User</h3>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
                       </div>
-                      <div class="row">
-                        <div class="col-6">
-                          <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                      <!-- form modal add users-->
+                      <?php echo form_open('admin/tambah_data_user',array('class'=>'form-horizontal','method'=>'post')); ?>
+                      <div class="modal-body">
+                        <div class="row">
+                          <div class="col-12">
+                            <div class="form-group">
+                              <label>Username</label>
+                              <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
+                            </div>
                           </div>
                         </div>
-                        <div class="col-6">
-                          <div class="form-group">
-                            <label>Re-Password</label>
-                            <input type="password" class="form-control" name="repassword" id="repassword" placeholder="Re-Password" required>
+                        <div class="row">
+                          <div class="col-6">
+                            <div class="form-group">
+                              <label>Password</label>
+                              <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                            </div>
+                          </div>
+                          <div class="col-6">
+                            <div class="form-group">
+                              <label>Re-Password</label>
+                              <input type="password" class="form-control" name="repassword" id="repassword" placeholder="Re-Password" required>
+                            </div>
                           </div>
                         </div>
-                      </div>
                         <div class="col-4">
                           <div class="form-group">
                             <input type="hidden" class="form-control" name="level" id="level" value="2">
@@ -231,7 +231,7 @@
                     document.getElementById("repassword").setCustomValidity("Passwords Tidak Sama");
                   else
                     document.getElementById("repassword").setCustomValidity('');
-                  }
+                }
               </script>
               <!-- ============ akhir modal add users ============ -->
               <!-- ============ MODAL HAPUS user =============== -->
@@ -242,13 +242,9 @@
                 <div class="modal fade" id="modal_hapus<?php echo $username;?>" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
                   <div class="modal-dialog">
                     <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-                        <h3 class="modal-title" id="myModalLabel">Hapus Customer</h3>
-                      </div>
-                      <?php echo form_open('admin/hapus_data_user',array('class'=>'form-horizontal','method'=>'post')); ?>
                       <div class="modal-body">
-                        <p>Anda yakin mau menghapus <b><?php echo $username;?></b></p>
+                        <?php echo form_open('admin/hapus_data_user',array('class'=>'form-horizontal','method'=>'post')); ?>
+                        <p>Anda yakin ingin menghapus <b><?php echo $username;?> ?</b></p>
                       </div>
                       <div class="modal-footer">
                         <input type="hidden" name="username" value="<?php echo $username;?>">
