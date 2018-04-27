48287d91bd0e4a262dc5d588cbf40af15aae0f0e
ivalrivall <ivalrival95@gmail.com>
2018-04-27 15:43:01 +0700

    Merge remote-tracking branch 'upstream/master'

--- application/views/js.php
+++ application/views/js.php
@@ -114,4 +114,3 @@
 <!-- Mirrored from pixinvent.com/apex-angular-4-bootstrap-admin-template/html-demo-2/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 22 Mar 2018 10:55:36 GMT -->
 
 </html>
-
